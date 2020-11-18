9380f0d3b1aa8dfde1fe2871a91a5a0bedd0fac0
V Smityukh <smityukh@gmail.com>
2020-11-18 21:43:54 +0200

    second commit

---
+++ db/migrate/20201113075720_create_tasks.rb
@@ -0,0 +1,11 @@
+class CreateTasks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.boolean :status
+      t.references :project, index: true
+
+      t.timestamps
+    end
+  end
+end
