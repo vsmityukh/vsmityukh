0e25a1cebee86ac4173e09787a79728e4c559acc
V Smityukh <smityukh@gmail.com>
2020-11-18 22:41:06 +0200

    prod save2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = 'http://localhost:3000'
+  config.action_controller.asset_host = 'https://vsmityukh.herokuapp.com/'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
